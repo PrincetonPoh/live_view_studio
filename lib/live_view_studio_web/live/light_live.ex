46be8ef099c66593ae2fc1066319beac46d77ddb
PohPrinceton <niceprinceton@gmail.com>
2023-02-11 09:18:51 -0800

    4. liveview lifecycle [completed]

---
+++ lib/live_view_studio_web/live/light_live.ex
@@ -0,0 +1,64 @@
+defmodule LiveViewStudioWeb.LightLive do
+  use LiveViewStudioWeb, :live_view
+
+  def mount(_params, _session, socket) do
+    socket = assign(socket, brightness: 10)
+    {:ok, socket}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <h1>Front Porch Light</h1>
+    <div id="light">
+      <div class="meter">
+        <span style={"width: #{@brightness}%"}>
+          <%= @brightness %>%
+        </span>
+      </div>
+      <button phx-click="off">
+        <img src="/images/light-off.svg" />
+      </button>
+      <button phx-click="down">
+        <img src="/images/down.svg" />
+      </button>
+      <button phx-click="up">
+        <img src="/images/up.svg" />
+      </button>
+      <button phx-click="on">
+        <img src="/images/light-on.svg" />
+      </button>
+      <br />
+      <br />
+      <button phx-click="fire">
+        <img src="/images/fire.svg" />
+      </button>
+    </div>
+    """
+  end
+
+  def handle_event("off", _, socket) do
+    socket = assign(socket, brightness: 0)
+    {:noreply, socket}
+  end
+
+  def handle_event("on", _, socket) do
+    socket = assign(socket, brightness: 100)
+    {:noreply, socket}
+  end
+
+  def handle_event("down", _, socket) do
+    brightness = max(socket.assigns.brightness - 10, 0)
+    socket = assign(socket, brightness: brightness)
+    {:noreply, socket}
+  end
+
+  def handle_event("up", _, socket) do
+    socket = update(socket, :brightness, &min(&1 + 10, 100))
+    {:noreply, socket}
+  end
+
+  def handle_event("fire", _, socket) do
+    socket = assign(socket, brightness: Enum.random(1..100))
+    {:noreply, socket}
+  end
+end
