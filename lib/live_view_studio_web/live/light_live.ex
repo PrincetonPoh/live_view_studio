ecb03bef9add9f1e3158c5da53f08fcfad3d73d5
PohPrinceton <niceprinceton@gmail.com>
2023-02-11 10:59:02 -0800

    5. dynamic form [completed]

--- lib/live_view_studio_web/live/light_live.ex
+++ lib/live_view_studio_web/live/light_live.ex
@@ -2,7 +2,13 @@ defmodule LiveViewStudioWeb.LightLive do
   use LiveViewStudioWeb, :live_view
 
   def mount(_params, _session, socket) do
-    socket = assign(socket, brightness: 10)
+    socket =
+      assign(
+        socket,
+        brightness: 10,
+        temp: "3000"
+      )
+
     {:ok, socket}
   end
 
@@ -11,7 +17,7 @@ defmodule LiveViewStudioWeb.LightLive do
     <h1>Front Porch Light</h1>
     <div id="light">
       <div class="meter">
-        <span style={"width: #{@brightness}%"}>
+        <span style={"width: #{@brightness}%; background: #{temp_color(@temp)}"}>
           <%= @brightness %>%
         </span>
       </div>
@@ -32,6 +38,31 @@ defmodule LiveViewStudioWeb.LightLive do
       <button phx-click="fire">
         <img src="/images/fire.svg" />
       </button>
+      <form phx-change="slider">
+        <input
+          type="range"
+          min="0"
+          max="100"
+          name="brightness"
+          value={@brightness}
+        />
+      </form>
+      <form phx-change="temperature">
+        <div class="temps">
+          <%= for temp <- ["3000", "4000", "5000"] do %>
+            <div>
+              <input
+                type="radio"
+                id={temp}
+                name="temp"
+                value={temp}
+                checked={temp == @temp}
+              />
+              <label for={temp}><%= temp %></label>
+            </div>
+          <% end %>
+        </div>
+      </form>
     </div>
     """
   end
@@ -61,4 +92,21 @@ defmodule LiveViewStudioWeb.LightLive do
     socket = assign(socket, brightness: Enum.random(1..100))
     {:noreply, socket}
   end
+
+  def handle_event("slider", params, socket) do
+    %{"brightness" => brightness} = params
+    socket = assign(socket, brightness: brightness)
+    {:noreply, socket}
+  end
+
+  def handle_event("temperature", params, socket) do
+    IO.inspect(params)
+    %{"temp" => temp} = params
+    socket = assign(socket, temp: temp)
+    {:noreply, socket}
+  end
+
+  defp temp_color("3000"), do: "#F1C40D"
+  defp temp_color("4000"), do: "#FEFF66"
+  defp temp_color("5000"), do: "#99CCFF"
 end
