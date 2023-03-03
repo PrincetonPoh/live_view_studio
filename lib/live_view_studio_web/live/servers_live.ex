3939b0e7ae2993f12d8de3a203ee9bcc77fec300
PohPrinceton <niceprinceton@gmail.com>
2023-03-03 12:58:51 -0800

    11. Live Navigation [completed]

--- lib/live_view_studio_web/live/servers_live.ex
+++ lib/live_view_studio_web/live/servers_live.ex
@@ -9,26 +9,42 @@ defmodule LiveViewStudioWeb.ServersLive do
     socket =
       assign(socket,
         servers: servers,
-        selected_server: hd(servers),
         coffees: 0
       )
 
     {:ok, socket}
   end
 
+  def handle_params(%{"id" => id}, _url, socket) do
+    {:noreply, assign(socket, selected_server: Servers.get_server!(id))}
+  end
+
+  def handle_params(_, _url, socket) do
+    IO.inspect(hd(socket.assigns.servers), label: "SERVER")
+    IO.inspect(3)
+
+    {:noreply,
+     assign(socket,
+       selected_server: hd(socket.assigns.servers)
+     )}
+  end
+
   def render(assigns) do
+    IO.inspect(self(), label: "RENDER")
+
     ~H"""
     <h1>Servers</h1>
     <div id="servers">
       <div class="sidebar">
         <div class="nav">
-          <a
+          <.link
             :for={server <- @servers}
+            patch={~p"/servers/#{server}"}
             class={if server == @selected_server, do: "selected"}
           >
             <span class={server.status}></span>
             <%= server.name %>
-          </a>
+          </.link>
         </div>
         <div class="coffees">
           <button phx-click="drink">
@@ -37,41 +53,51 @@ defmodule LiveViewStudioWeb.ServersLive do
           </button>
         </div>
       </div>
-      <div class="main">
-        <div class="wrapper">
-          <div class="server">
-            <div class="header">
-              <h2><%= @selected_server.name %></h2>
-              <span class={@selected_server.status}>
-                <%= @selected_server.status %>
-              </span>
-            </div>
-            <div class="body">
-              <div class="row">
-                <span>
-                  <%= @selected_server.deploy_count %> deploys
-                </span>
-                <span>
-                  <%= @selected_server.size %> MB
-                </span>
-                <span>
-                  <%= @selected_server.framework %>
-                </span>
-              </div>
-              <h3>Last Commit Message:</h3>
-              <blockquote>
-                <%= @selected_server.last_commit_message %>
-              </blockquote>
-            </div>
-          </div>
-          <div class="links"></div>
-        </div>
-      </div>
+      <.server selected_server={@selected_server} />
     </div>
     """
   end
 
   def handle_event("drink", _, socket) do
     {:noreply, update(socket, :coffees, &(&1 + 1))}
+  end
+
+  attr :selected_server, :any, required: true
+
+  def server(assigns) do
+    ~H"""
+    <div class="main">
+      <div class="wrapper">
+        <div class="server">
+          <div class="header">
+            <h2><%= @selected_server.name %></h2>
+            <span class={@selected_server.status}>
+              <%= @selected_server.status %>
+            </span>
+          </div>
+          <div class="body">
+            <div class="row">
+              <span>
+                <%= @selected_server.deploy_count %> deploys
+              </span>
+              <span>
+                <%= @selected_server.size %> MB
+              </span>
+              <span>
+                <%= @selected_server.framework %>
+              </span>
+            </div>
+            <h3>Last Commit Message:</h3>
+            <blockquote>
+              <%= @selected_server.last_commit_message %>
+            </blockquote>
+          </div>
+        </div>
+        <div class="links">
+          <.link navigate={~p"/light"}>Adjust light</.link>
+        </div>
+      </div>
+    </div>
+    """
   end
 end
